8c85d5f6472cff1dd888a8fd17e54421bacc34b0
Mazen Melouk <mazen.melouk@zalando.de>
2017-07-08 14:50:45 +0200

    inital setup

---
+++ src/main/java/com/challenge/transactions/DummyApplication.java
@@ -0,0 +1,14 @@
+package com.challenge.transactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class DummyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DummyApplication.class, args);
+	}
+}
